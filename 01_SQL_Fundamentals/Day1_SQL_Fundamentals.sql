406240b5e288412cbd3136b0ffa1aa2ee779659e
Avantika Sharad Kaloge <avanti1616k@gmail.com>
2026-08-02 17:56:01 +0530

    Add Day 1 SQL Fundamentals
    Initial SQL script for SQL Fundamentals Day 1, including a sample query to display all columns from the employee table.

---
+++ 01_SQL_Fundamentals/Day1_SQL_Fundamentals.sql
@@ -0,0 +1,29 @@
+/*
+========================================================
+Topic      : SQL Fundamentals
+Day        : 1
+Database   : SQL Server (SSMS)
+Author     : Avantika Sharad Kaloge
+========================================================
+*/
+
+/*
+--------------------------------------------------------
+SELECT
+--------------------------------------------------------
+*/
+
+/*
+Question 1:
+Display all columns from the employee table.
+*/
+
+SELECT *
+FROM emp;
+
+/*
+--------------------------------------------------------
+More questions and explanations will be added as I
+continue my SQL Interview Bootcamp.
+--------------------------------------------------------
+*/
